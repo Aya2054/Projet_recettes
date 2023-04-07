162901437db87432bb91d76f2c1f833987d52462
Aya BERRADI <93614764+Aya2054@users.noreply.github.com>
2023-04-07 10:33:06 +0200

    Update Template.php

--- PHP/Class/Template.php
+++ PHP/Class/Template.php
@@ -19,7 +19,7 @@ class Template
                     integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0"
                     crossorigin="anonymous"></script>
 
-            <link rel="stylesheet" href="<?php echo $GLOBALS['CSS_DIR'] ?>gamedb.css">
+            <link rel="stylesheet" href="<?php echo $GLOBALS['CSS_DIR'] ?>style.css">
 
         </head>
         <body>
