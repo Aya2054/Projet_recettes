4a28f4585161178b82da807b89ea9a56fbc49891
Aya BERRADI <93614764+Aya2054@users.noreply.github.com>
2023-04-07 13:36:14 +0200

    Update Template.php

--- PHP/Class/Template.php
+++ PHP/Class/Template.php
@@ -18,6 +18,8 @@ class Template
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"
                     integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0"
                     crossorigin="anonymous"></script>
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.6.0/font/bootstrap-icons.css">
+
 
             <link rel="stylesheet" href="<?php echo $GLOBALS['CSS_DIR'] ?>style.css">
 
