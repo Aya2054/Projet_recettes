d33fb36dbb0b2ab339d81f871cd1ded12e8d1ab1
Aya BERRADI <93614764+Aya2054@users.noreply.github.com>
2023-04-07 10:32:14 +0200

    Update header.php

--- PHP/pages/header.php
+++ PHP/pages/header.php
@@ -1,1 +1,35 @@
-<?php
+<header>
+    <nav>
+        <div id="top-bar">
+            <div id="notice">
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            </div>
+            <div id="contact">
+                <div class="tel">
+                    <img src="icons/phone-solid.svg" alt="phone" class="icons" id="t">
+                    <a href="tel:(07)668888339"> (07) 66 88 33 99</a>
+                </div>
+                <div class="mail">
+                    <img src="icons/envelope-solid.svg" alt="email" class="icons" id="m">
+                    <a href="mail:isa-office@gmail.com"> isa-office@gmail.com</a>
+                </div>
+            </div>
+        </div>
+        <div id="menu">
+            <div><a href=""><h1 id="logo" class="elm-menu"><span id="isa">ISA</span>-Recettes</h1></a>
+
+            </div>
+            <div>
+                <ul>
+                    <li><a href="#" class="elm-menu">Acceuil</a></li>
+                    <li><a href="#" class="elm-menu">Nos spécialités</a></li>
+                    <li><a href="#" class="elm-menu">Nos recettes</a></li>
+                    <li><a href="#" class="elm-menu">Opérations</a></li>
+                </ul>
+            </div>
+            <div>
+                <input type="button" value="Connexion" id="btn">
+            </div>
+        </div>
+    </nav>
+</header>
