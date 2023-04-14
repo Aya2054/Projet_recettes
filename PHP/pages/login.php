900c1755317ede726d0a971fdfe26186f98077ca
Aya BERRADI <93614764+Aya2054@users.noreply.github.com>
2023-04-14 09:35:45 +0200

    Update login.php

--- PHP/pages/login.php
+++ PHP/pages/login.php
@@ -1,1 +1,39 @@
 <?php
+require __DIR__."/../Config/config.php" ;
+
+session_start() ;
+
+require ".." . DIRECTORY_SEPARATOR .'class'.DIRECTORY_SEPARATOR.'Autoloader.php' ;
+Autoloader::register();
+
+use login\Logger ;
+
+
+$logger = new Logger() ;
+
+$username = null;
+$password = null ;
+if (isset($_POST['username']) and isset($_POST['password'])){
+    $username = $_POST['username'] ;
+    $password = $_POST['password'] ;
+    $response = $logger->log(trim($username), trim($password)) ;
+    if ($response['granted']){
+        $_SESSION['nickname'] = $response['nick'] ;
+        header("Location: ../index.php");
+        exit() ;
+    }
+}
+
+ob_start() ;
+if (!isset($response)) :
+    echo "VIDE";
+    $logger->generateLoginForm("", $username);
+elseif (!$response['granted']) :
+    echo "ERREUR";
+    echo "<div>" .$response['error']."</div>" ;
+    $logger->generateLoginForm("", $username, $response['error']);
+endif;
+
+
+$code = ob_get_clean() ;
+Template::render($code);
