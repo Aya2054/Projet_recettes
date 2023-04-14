98600cbd20c1a3f15337d7b56a89c632693902b4
Aya BERRADI <93614764+Aya2054@users.noreply.github.com>
2023-04-14 10:20:34 +0200

    Update header.php

--- PHP/pages/header.php
+++ PHP/pages/header.php
@@ -4,21 +4,6 @@ $logged = isset($_SESSION['nickname']) ;
 
 <header>
     <nav>
-        <div id="top-bar">
-            <div id="notice">
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
-            </div>
-            <div id="contact">
-                <div class="tel">
-                    <i class="bi bi-telephone"></i>
-                    <a href="tel:(07)668888339" id="t"> (07) 66 88 33 99</a>
-                </div>
-                <div class="mail">
-                    <i class="bi bi-envelope"></i>
-                    <a href="mail:isa-office@gmail.com" id="m"> isa-office@gmail.com</a>
-                </div>
-            </div>
-        </div>
         <div id="menu">
             <div><a href=""><h1 id="logo" class="elm-menu"><span id="isa">ISA</span>-Recettes</h1></a>
 
@@ -37,17 +22,6 @@ $logged = isset($_SESSION['nickname']) ;
                     </li>
 
 
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Nos spéacialités
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
-                        </ul>
-                    </li>
-
 
 
                     <li class="nav-item dropdown">
@@ -64,7 +38,7 @@ $logged = isset($_SESSION['nickname']) ;
                     <?php if($logged):?>
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Nos opérations
+                            Opérations
                         </a>
                         <ul class="dropdown-menu">
                             <li><a class="dropdown-item" href="#">Action</a></li>
