2cbc5d29137bb74a6fac75833676f311edf4f06e
Aya BERRADI <93614764+Aya2054@users.noreply.github.com>
2023-05-05 08:53:07 +0200

    Update DB_config.php

--- PHP/Config/DB_config.php
+++ PHP/Config/DB_config.php
@@ -3,4 +3,4 @@ $GLOBALS['db_name'] = 'gamesdb';
 $GLOBALS['db_host'] = '127.0.0.1';
 $GLOBALS['db_port'] = '3306';
 $GLOBALS['db_user'] = 'root';
-$GLOBALS['db_pwd'] = 'root';
+$GLOBALS['db_pwd'] = '';
