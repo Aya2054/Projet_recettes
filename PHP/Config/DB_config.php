3fbb0987c96a137978ea379427d9a37cdaaf338d
Aya BERRADI <93614764+Aya2054@users.noreply.github.com>
2023-05-05 10:18:47 +0200

    Update DB_config.php

--- PHP/Config/DB_config.php
+++ PHP/Config/DB_config.php
@@ -1,6 +1,6 @@
 <?php
-$GLOBALS['db_name'] = 'gamesdb';
-$GLOBALS['db_host'] = '127.0.0.1';
+$GLOBALS['db_name'] = 'isa_cuisine';
+$GLOBALS['db_host'] = 'localhost';
 $GLOBALS['db_port'] = '3306';
 $GLOBALS['db_user'] = 'root';
 $GLOBALS['db_pwd'] = '';
