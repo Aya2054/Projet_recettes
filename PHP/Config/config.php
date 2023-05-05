bd9dc0b30fba5a53f6c4267fc89e48d2b8f9ce7c
Aya BERRADI <93614764+Aya2054@users.noreply.github.com>
2023-05-05 15:58:51 +0200

    Update config.php

--- PHP/Config/config.php
+++ PHP/Config/config.php
@@ -1,5 +1,5 @@
 <?php
-$GLOBALS['PHP_DIR'] = __DIR__ . "/";
+$GLOBALS['PHP_DIR'] = __DIR__ . "/../";
 $GLOBALS['DOCUMENT_DIR'] = "./";
 $GLOBALS['IMG_DIR'] = $GLOBALS['DOCUMENT_DIR'] . "../../../images/";
 $GLOBALS['CSS_DIR'] = $GLOBALS['DOCUMENT_DIR'] . "../../../CSS/";
