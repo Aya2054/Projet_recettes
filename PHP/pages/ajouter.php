fba89ed97faea521c6e4e630586e3ee9b069a4e7
Aya BERRADI <93614764+Aya2054@users.noreply.github.com>
2023-05-11 14:42:45 +0200

    Update ajouter.php

--- PHP/pages/ajouter.php
+++ PHP/pages/ajouter.php
@@ -14,14 +14,49 @@ $gf = new \gdb\recetteForm();
 if (empty($_POST['title'])) {
     $gf->generateForm();
 } else {
+    //recuperer l'image de la recette
     $imgFile = isset($_FILES['image']) ? $_FILES['image'] : null;
-    if ($imgFile != null) {
-        $gf->createRecette($_POST['title'], $_POST['description'], $imgFile);
-    } else {
-        echo "img vide";
-    }
-}
 
+    //recupérer les ingredients
+    if($_SERVER['REQUEST_METHOD'] === 'POST') {
+        // Récupérer les données des ingrédients depuis le formulaire
+        $ingredientData = $_POST['ingredients'];
+        $tagsData=$_POST['tags'];
+
+        // Tableau pour stocker les ingrédients
+        $ingredients = array();
+        $tags= array();
+
+        // Parcourir chaque ingrédient
+        foreach ($ingredientData as $data) {
+            $i = 0;
+            $name = $data['name'];
+            $quantity = $data['quantity'];
+            $unit = $data['unit'];
+            $image = isset($_FILES['ingredients[' . $i . '][image]']) ? $_FILES['ingredients[' . $i . '][image]'] : null;
+
+            // Créer une instance d'ingrédient et l'ajouter au tableau
+            $ingredient = new \gdb\ingredient($name, $quantity, $unit, $image);
+            $ingredients[] = $ingredient;
+            $i++;
+        }
+
+        //parcourir chaque tags
+        foreach ($tagsData as $data) {
+
+            // Créer une instance de tag et l'ajouter au tableau
+            $tag = new \gdb\tag($data);
+            $tags[] = $tag;
+        }
+
+        if ($imgFile != null) {
+            $gf->createRecette($_POST['title'], $_POST['description'], $imgFile, $ingredients, $tags);
+        } else {
+            echo "img vide";
+        }
+    }
+
+}
 ?>
 
 <?php $content = ob_get_clean() ?>
