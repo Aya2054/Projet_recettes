0464992cb1fca5f01127818bb8f5ca0ad8785f9e
Aya BERRADI <93614764+Aya2054@users.noreply.github.com>
2023-05-11 14:40:28 +0200

    Update recetteForm.php

--- PHP/Class/gdb/recetteForm.php
+++ PHP/Class/gdb/recetteForm.php
@@ -11,7 +11,7 @@ class recetteForm
 
     public function generateForm()
     { ?>
-        <section>
+        <section id="form-ajouter">
             <h1>Ajouter une recette</h1>
             <form method="POST" enctype="multipart/form-data">
                 <label for="title">Titre :</label>
@@ -21,10 +21,64 @@ class recetteForm
                 <textarea id="description" name="description" required></textarea>
 
                 <label for="image">Image :</label>
-                <input type="file" id="image" name="image" accept="image/*" required>
+                <input type="file" id="image-input" name="image" accept="image/*" required>
+                <div id="preview-container">
+                    <img id="preview-image" src="">
+                </div>
 
-                <label for="ingredients">Ingrédients :</label>
-                <textarea id="ingredients" name="ingredients" required></textarea>
+                <div id="divIngredients" class="mt20 repeator-ingredients">
+                    <div class="row repeator-item">
+                        <div class="large-2 medium-2 small-6 columns">
+                            <label>Quantité</label>
+                            <input type="text" name="quantite" value="" />
+                        </div>
+                        <div class="large-3 medium-3 small-6 columns">
+                            <label>Mesures</label>
+                            <select class="auto-mesure" name="mesure[0]">
+                                <option value="">(Rien)</option>
+                                <option value="grammes">grammes (g)</option>
+                                <option value="kilogrammes">kilogrammes (kg)</option>
+                                <option value="litres">litres (l)</option>
+                                <option value="millilitres">millilitres (ml)</option>
+                                <option value="centilitres">centilitres (cl)</option>
+                            </select>
+                        </div>
+                        <div class="large-6 medium-6 small-10 columns">
+                            <label for="ingredient1">Ingrédient :</label>
+                            <select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example" id="ingredient1" name="ingredient1">
+                                <option selected>Choisir un ingredient</option>
+                                <?php
+                                $ingredient = new \gdb\ingredient();
+                                $ingredients = $ingredient->generer_auto();
+                                foreach ($ingredients as $ingr) {
+                                    echo '<option value="' . $ingr->id . '">' . $ingr->nom . '</option>';
+                                }
+                                ?>
+                            </select>
+                        </div>
+
+                        <div class="columns">
+                            <div class="pb15 row norepeat">
+                                <div class="large-2 medium-2 small-6 columns">
+                                    <span class="help-text">ex: 120</span>
+                                </div>
+                                <div class="large-3 medium-3 small-6 columns">
+                                    <span class="help-text">grammes (gr)</span>
+                                </div>
+                                <div class="large-7 medium-7 small-10 columns">
+                                    <span class="help-text">de farine</span>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="large-1 medium-1 small-2 columns">
+                            <a href="#" class="button bg-light-gray small icon-cross circle delete repeator-delete"></a>
+                        </div>
+                    </div>
+                    <button type="button" class="btn btn-primary">Ajouter un ingredient</button>
+
+                </div>
+
 
                 <label for="tags">Tags :</label>
                 <input type="text" id="tags" name="tags" required>
@@ -32,10 +86,71 @@ class recetteForm
                 <input type="submit" value="Ajouter">
             </form>
         </section>
+
+        <script>
+            document.addEventListener('DOMContentLoaded', function (){
+                // prévisualisation de l'image
+                const imageInput = document.getElementById('image-input');
+                const previewImage = document.getElementById('preview-image');
+
+                imageInput.addEventListener('change', function() {
+                    const file = this.files[0];
+                    const reader = new FileReader();
+
+                    reader.addEventListener('load', function() {
+                        previewImage.src = reader.result;
+                    });
+
+                    if (file) {
+                        reader.readAsDataURL(file);
+                    } else {
+                        previewImage.src = "";
+                    }
+                });
+
+                // vérification du formulaire
+                let form = document.querySelector("form");
+                let name = document.getElementById("title");
+
+                form.addEventListener('submit', (ev => {
+                    if (name.value == ""){
+                        ev.preventDefault()
+                        name.classList.add("error") ;
+                    }
+                }))
+
+                name.addEventListener('keydown', (ev => {
+                    name.classList.remove("error") ;
+                })
+                // Récupérer le bouton d'ajout d'ingrédient
+                var addButton = document.querySelector("#divIngredients button");
+
+                // Ajouter un gestionnaire d'événements click sur le bouton
+                addButton.addEventListener("click", function() {
+                    // Récupérer le parent div contenant tous les ingrédients
+                    var parentDiv = document.querySelector("#divIngredients");
+
+                    // Récupérer le premier élément de la liste d'ingrédients (à cloner)
+                    var ingredientDiv = document.querySelector(".repeator-item");
+
+                    // Cloner l'élément et ses enfants
+                    var newIngredientDiv = ingredientDiv.cloneNode(true);
+
+                    // Effacer les champs de saisie
+                    newIngredientDiv.querySelectorAll("input, select").forEach(function(input) {
+                        input.value = "";
+                    });
+
+                    // Ajouter le nouvel élément en tant qu'enfant du parent div
+                    parentDiv.appendChild(newIngredientDiv);
+                });
+            })
+
+        </script>
         <?php
     }
 
-    public function createRecette($titre, $description = null, $imgFile = null, $ingredients = null)
+    public function createRecette($titre, $description = null, $imgFile = null, $ingredients = null, $tag=null)
     {
         if ($this->gdb == null) $this->gdb = new recette();
         $this->gdb->create_recette($titre, $description, $imgFile, $ingredients);
