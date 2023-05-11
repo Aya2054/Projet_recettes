060be2d2bd901181a620eecec3f0bc618d9aeaef
Aya BERRADI <93614764+Aya2054@users.noreply.github.com>
2023-05-12 01:01:46 +0200

    Update recetteForm.php

--- PHP/Class/gdb/recetteForm.php
+++ PHP/Class/gdb/recetteForm.php
@@ -109,6 +109,7 @@ class recetteForm
                         ?>
                     </datalist>
                 </div>
+                <br>
 
                 <div></div>
                 <br>
@@ -117,6 +118,8 @@ class recetteForm
         </section>
 
         <script>
+
+
             // Récupération de l'élément input de type file
             const inputImage = document.getElementById("image");
 
@@ -159,6 +162,7 @@ class recetteForm
             const addButton = document.querySelector('.ingredient-add');
             const removeButton = document.querySelector('.ingredient-remove');
             const container = document.querySelector('.ingredient-container');
+            let i = 0;
 
             // Fonction qui ajoute une nouvelle ligne d'ingrédient
             function addIngredient() {
@@ -166,21 +170,19 @@ class recetteForm
                 const ingredientInput = document.createElement('div');
                 ingredientInput.classList.add('ingredient-container');
                 ingredientInput.innerHTML = `
-  <div class="ingredient-title">
-                        <div class="ingredient-container">
-                            <div class="ingredient-input">
-                                <select name="ingredients[0][unit]">
-                                    <option value="g">grammes (g)</option>
-                                    <option value="ml">kilogrammes (kg)</option>
-                                    <option value="ml">litres (l)</option>
-                                    <option value="ml">millilitres (ml)</option>
-                                </select>
-                            </div>
-                            <div class="ingredient-input">
-                                <input type="text" name="ingredients[0][quantity]" placeholder="Quantité">
-                            </div>
-                            <div class="ingredient-input">
-                                <input list="list-ingredient" name="ingredients[0][name]">
+<div class="ingredient-input">
+  <select name="ingredients[${i}][unit]">
+    <option value="g">g</option>
+    <option value="ml">ml</option>
+    <option value="kg">kg</option>
+    <option value="l">l</option>
+  </select>
+</div>
+<div class="ingredient-input">
+  <input type="text" name="ingredients[${i}][quantity]" placeholder="Quantité">
+</div>
+<div class="ingredient-input">
+                                <input list="list-ingredient" name="ingredients[${i}][name]">
                                 <datalist id="list-ingredient">
                                     <?php
                 $ingredient = new \gdb\ingredient();
@@ -192,14 +194,15 @@ class recetteForm
                 ?>
                                 </datalist>
                             </div>
-
-                            <div class="ingredient-image">
-                                <input type="file" id="image-ing" name="ingredients[0][image]" accept="image/*" required>
-                            </div>
-                        </div>`;
+<div class="ingredient-image">
+  <input type="file" name="ingredients[${i}][image]" accept="image/*">
+</div>
+`;
 
                 // Ajout de la nouvelle ligne d'ingrédient dans le conteneur
                 container.appendChild(ingredientInput);
+
+                i++;
             }
 
             // Fonction qui supprime la dernière ligne d'ingrédient
@@ -211,6 +214,7 @@ class recetteForm
                 if (ingredientInputs.length > 1) {
                     const lastIngredientInput = ingredientInputs[ingredientInputs.length - 1];
                     lastIngredientInput.remove();
+                    i--;
                 }
             }
 
@@ -231,51 +235,72 @@ class recetteForm
 
 
 
+
             // Récupération des éléments HTML pour la gestion des tags
             const addButton0 = document.querySelector('.tag-add');
             const removeButton0 = document.querySelector('.tag-remove');
             const tagContainer = document.querySelector('.tag-container');
             const tags = [];
 
-            // Écouteur d'événement sur le bouton "+"
+            // Ajout d'un écouteur d'événement pour le bouton "+"
             addButton0.addEventListener('click', () => {
-                // Création d'un nouvel élément d'entrée de balise
+                // Ajout d'un nouvel élément de saisie de tag au tableau
                 const newTagInput = document.createElement('input');
                 newTagInput.type = 'text';
                 newTagInput.name = 'tags';
                 newTagInput.required = true;
+                newTagInput.setAttribute('list', `tag-list-${tags.length}`);
+
+                // Création d'une datalist pour le nouveau tag input
+                const dataListHTML = `
+    <datalist id="tag-list-${tags.length}">
+                        <?php
+                $tag = new \gdb\tag();
+                $tags = $tag->generer_auto();
+                foreach ($tags as $t) {
+                    echo '<option value="' . $t->nom . '">';
+                }
+                ?>
+                    </datalist>
+  `;
 
                 // Ajout du nouvel élément d'entrée de balise au conteneur
-                tagContainer.appendChild(newTagInput);
+                tagContainer.innerHTML += newTagInput.outerHTML + dataListHTML;
 
                 // Ajout du tag au tableau
                 tags.push(newTagInput);
+
+                // Mettre à jour l'affichage des tags
+                updateTags();
             });
 
-            // Ajout d'un écouteur d'événement pour le bouton de suppression
+            // Ajout d'un écouteur d'événement pour le bouton "-"
             removeButton0.addEventListener('click', () => {
                 // Suppression du dernier élément du tableau de tags
                 tags.pop();
 
                 // Suppression du dernier élément du conteneur de tags
                 tagContainer.lastChild.remove();
+                tagContainer.lastChild.remove();
+
+                // Mettre à jour l'affichage des tags
+                updateTags();
             });
 
             // Fonction pour mettre à jour l'affichage des tags
             function updateTags() {
-                const tagsList = document.createElement('ul'); // Crée un élément HTML <ul> pour afficher la liste des tags
-                tags.forEach(tag => {
-                    const tagItem = document.createElement('li'); // Crée un élément HTML <li> pour chaque tag
-                    tagItem.textContent = tag.value; // Ajoute le texte du tag à l'élément <li>
-                    tagsList.appendChild(tagItem); // Ajoute l'élément <li> à l'élément <ul>
-                });
-                tagContainer.appendChild(tagsList); // Ajoute l'élément <ul> au conteneur de tags
+                // Ne fait rien, on utilise maintenant une datalist pour afficher les tags
+            }
+
+            // Fonction utilitaire pour créer une option dans une datalist
+            function createOption(value) {
+                const option = document.createElement('option');
+                option.value = value;
+                return option;
             }
 
             // Mettre à jour l'affichage des tags au chargement de la page
             updateTags();
-
-
         </script>
         <?php
     }
