e282ed75d7189ca127f1a5c0975650d17e841bda
Aya BERRADI <93614764+Aya2054@users.noreply.github.com>
2023-05-12 22:55:44 +0200

    Update supp.php

--- PHP/pages/supp.php
+++ PHP/pages/supp.php
@@ -1,1 +1,15 @@
 <?php
+require_once "../Config/config.php";
+
+require ".." . DIRECTORY_SEPARATOR . 'class' . DIRECTORY_SEPARATOR . 'Autoloader.php';
+Autoloader::register();
+
+$gf = new \gdb\recetteForm();
+?>
+
+    <!-- Démarre le buffering -->
+<?php ob_start() ?>
+
+<?php
+$id=$_GET['id'];
+$gf->deleteRecette($id);
